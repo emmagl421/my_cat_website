9efe408eaaf445aac76e31444a57a39d2f9f218f
Emma Leonard <lemma01245@gmail.com>
2026-09-23 10:31:49 -0400

    added a button to another page and + the return button from that page

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.querySelector('#nugget').addEventListener('click', function() {
+
+//Add listener for return button
+document.getElementById('nugget')?.addEventListener('click', function() {
     window.location.href = "love_cats.html";
+});
+
+
+//Add listener for return button
+document.getElementById('retButton')?.addEventListener('click', function() {
+    window.location.href = "index.html";
 });
